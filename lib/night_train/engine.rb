f1c09b02bb6648fe10bbabaac0f4033d33c963c0
Karen Lundgren <karen.e.lundgren@gmail.com>
2015-07-24 08:51:11 -0600

    Removed test_unit, erb, and mongoid namespaces from `rails generate` output

--- lib/night_train/engine.rb
+++ lib/night_train/engine.rb
@@ -2,6 +2,7 @@ module NightTrain
   class Engine < ::Rails::Engine
     isolate_namespace NightTrain
     config.generators do |g|
+      g.hidden_namespaces << :test_unit << :erb << :mongoid
       g.orm             :active_record
       g.template_engine :haml
       g.test_framework  :rspec, fixture: false
